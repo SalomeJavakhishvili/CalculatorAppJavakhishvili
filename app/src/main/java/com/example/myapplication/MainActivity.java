79f7dc88d25ade8e6f9e4f83c9174a818aaf888b
SalomeJavakhishvili <113720709+SalomeJavakhishvili@users.noreply.github.com>
2023-09-11 11:56:29 -0500

    Minor edits

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,8 +12,4 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public static void main(String[] args) {
-
-        public void ButtonToInput
-    }
 }
